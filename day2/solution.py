3e1eeb2d634f91105d58a849e8cd24c3e5487616
Mike Hearing <miketheredherring@gmail.com>
2024-12-02 23:08:22 -0500

    Day 2 part 1

---
+++ day2/solution.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+def get_reports(filename):
+    reports = []
+    with open(filename, 'r') as f:
+        for line in f:
+            reports.append(list(map(int, line.strip().split(' '))))
+    return reports
+
+def report_is_valid(report):
+    def comp(a, b):
+        diff = abs(a - b)
+        if 1 <= diff <= 3:
+            return True
+        return False
+
+    if not(report == sorted(report) or report == sorted(report, reverse=True)):
+        return False
+
+    lr = len(report)
+    valid = True
+    for i in range(lr):
+        lni = max(0, i - 1)
+        rni = min(lr - 1, i + 1)
+        
+        # Ensure no large jumps
+        if lni != i:
+            valid &= comp(report[lni], report[i])
+        if rni != i:
+            valid &= comp(report[rni], report[i])
+        
+        if not valid:
+            return False
+    return True
+
+if __name__ == '__main__':
+    reports = get_reports('input.txt')
+    count = 0
+    for report in reports:
+        if report_is_valid(report):
+            count += 1
+    print(count)
